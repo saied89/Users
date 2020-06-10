4706b268ecfb2407e5e014016f95af50f61a0c4c
saied <saiedmomen@gmail.com>
2020-06-10 10:42:10 +0430

    setup navigation and actionbar

--- app/src/main/java/com/saied/users/MainActivity.kt
+++ app/src/main/java/com/saied/users/MainActivity.kt
@@ -2,10 +2,22 @@ package com.saied.users
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.findFragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.saied.users.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupActionBarWithNavController(
+            findNavController(R.id.nav_host_fragment)
+        )
     }
 }
