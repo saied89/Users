075ce2bdda754e0e15c7c671c3f9043d85e689df
saied <saiedmomen@gmail.com>
2020-06-10 11:46:10 +0430

    setup koin

--- app/src/main/java/com/saied/users/App.kt
+++ app/src/main/java/com/saied/users/App.kt
@@ -1,9 +1,15 @@
 package com.saied.users
 
 import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        startKoin {
+            androidContext(this@App)
+        }
     }
 }
