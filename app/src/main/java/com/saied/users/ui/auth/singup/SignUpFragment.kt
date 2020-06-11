b7d34362daef1f58492c8cf977ce74826b0816d5
saied <saiedmomen@gmail.com>
2020-06-12 04:29:12 +0430

    add navigate after successful user creation

--- app/src/main/java/com/saied/users/ui/auth/singup/SignUpFragment.kt
+++ app/src/main/java/com/saied/users/ui/auth/singup/SignUpFragment.kt
@@ -8,9 +8,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.google.android.material.snackbar.Snackbar
 import com.saied.users.databinding.FragmentSignUpBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -109,8 +111,8 @@ class SignUpFragment : Fragment() {
             _binding?.cPasswordInputLayout?.error = null
             _binding?.fnameInputLayout?.error = null
             it.fold(onSuccess = {
-                // TODO replace with actual code
-                showSnackabr("Success!")
+                Toast.makeText(context, "User created!", Toast.LENGTH_SHORT).show()
+                val res = findNavController().navigateUp()
             }, onFailure = { exception ->
                 when (exception) {
                     is SignUpValidationException.EmailValidationException ->
